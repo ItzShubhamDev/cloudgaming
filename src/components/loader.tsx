54f657f2d09f929c8a7939a2668c6e4f1d58fc01
Shubham <shubhamdev01@outlook.com>
2024-11-02 13:22:06 +0530

    Test

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react";
 export default function Loader() {
   return (
     <>
-      <div className="animate-spin text-blue-500">
+      <div className="animate-spin text-blue-500 h-8 w-8">
         <LoaderCircle />
       </div>
       <span className="sr-only">Loading...</span>
